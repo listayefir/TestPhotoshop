8d22234545e9691536fe124ece2d602dac56e762
Butters <butters@mail.ru>
2016-06-20 13:53:56 +0300

    Добавлен класс Pixel.cs, введена проверка целостности (значение канала пикселя в пределах [0,1])

---
+++ Data/Pixel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class Pixel
+    {
+        private double r;
+        private double g;
+        private double b;
+
+        public double R
+        { get { return r; }
+          set { r = CheckChannel(value); }         
+        }
+
+        public double G
+        {
+            get { return g; }
+            set { g = CheckChannel(value); }
+        }
+        
+        public double B
+        {
+            get { return b; }
+            set { b = CheckChannel(value); }
+        }
+
+        public Pixel()
+        {
+
+        }
+
+        public double CheckChannel(double value)
+        {
+            if (value < 0 || value > 1)
+                throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value));
+            else return value;
+        }
+       
+    }
+}
